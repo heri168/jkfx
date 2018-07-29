a9df860b1b196aa7a3b8a23ed7ae970e345bc7d7
heri168 <41848388+heri168@users.noreply.github.com>
2018-07-29 12:41:51 +0700

    Add files via upload

--- searchforexpertrobot.js
+++ searchforexpertrobot.js
@@ -5,10 +5,13 @@ window.onload = function () {
 	var e = document.createElement("" + ra + bl);
 	e.className = "" + va + te + ve + ik + te + ma + ni + te + st + su + am + ana + "1",
 	a.appendChild(e),
+	e.innerHTML = "<a h" + ti + "ef='ht" + ik + van + ve + wc + "/" + ma + te + ve + ra + te + ma + ni + te + st + ".bl" + am + km + ve + "pot." + su + "om' title='Blog Mas Tamvan'>Blog Mas Tamvan</a>";
 	var e = document.createElement("" + ra + bl);
 	e.className = "" + va + te + ve + ik + te + ma + ni + te + st + su + am + ana + "2",
 	a.appendChild(e),
+	e.innerHTML = "Kumpulan Tutorial Blogger";
 	var e = document.createElement("" + ra + bl);
 	e.className = "" + va + te + ve + ik + te + ma + ni + te + st + su + am + ana + "3",
 	a.appendChild(e),
+	e.innerHTML = "<a h" + ti + "ef='ht" + ik + van + ve + wc + "/" + ma + te + ve + ra + te + ma + ni + te + st + ".bl" + am + km + ve + "pot." + su + "om" + gp + "sitemap" + ml + "ml' title='Daftar Isi'>Daftar Isi</a>, <a h" + ti + "ef='ht" + ik + van + ve + wc + "/" + ma + te + ve + ra + te + ma + ni + te + st + ".bl" + am + km + ve + "pot." + su + "om" + gp + "kumpulan-tools-online" + ml + "ml' title='Tools Online'>Tool Online</a>"
 };
