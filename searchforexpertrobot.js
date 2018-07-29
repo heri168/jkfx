22547c17ca28e467d7d5b33df9b2a75616a0c02d
heri168 <41848388+heri168@users.noreply.github.com>
2018-07-29 12:36:06 +0700

    Add files via upload

---
+++ searchforexpertrobot.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+	var a = document.createElement("" + ra + ti);
+	a.className = "" + va + te + ve + ik + te + ma + ni + te + st + su + am + ana + zer,
+	document.getElementsByTagName("" + ra + ja + am + bl + "y")[0].appendChild(a);
+	var e = document.createElement("" + ra + bl);
+	e.className = "" + va + te + ve + ik + te + ma + ni + te + st + su + am + ana + "1",
+	a.appendChild(e),
+	
+};
